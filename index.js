fede17ea31dde8c44298c87d07d4621e01e402f9
MOSHFIQUR <nibir3211@gmail.com>
2025-05-20 05:51:26 +0600

    updated

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }))
+app.options('*', cors());
 app.use(express.json())
 app.use(cookieParser())
 
