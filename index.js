4dc6e845e55c83f650eb625b3fb8c41eb64aa4cc
MOSHFIQUR <nibir3211@gmail.com>
2025-05-22 12:31:04 +0600

    finished the server side

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(cors({
     origin: [
         'https://innora-server-side.vercel.app',
         "https://innora-client-side-1.vercel.app",
-        "https://innora-client-side-2.vercel.app",
-        'http://localhost:3000',
-        'http://localhost:3001',
-
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
