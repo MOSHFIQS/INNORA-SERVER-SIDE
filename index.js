a24a84d450804b1e902121d37191f25524f9fa04
MOSHFIQUR <nibir3211@gmail.com>
2025-05-22 15:06:35 +0600

    updated

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors({
     origin: [
         'https://innora-server-side.vercel.app',
         "https://innora-client-side-1.vercel.app",
+        'http://localhost:3000',
+        'http://localhost:3001',
+
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
